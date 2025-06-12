928c5de9af38b0074bf986ffa48d53cdfcba13a6
Connor Laber <connorlaber1@gmail.com>
2025-06-12 12:19:17 -0600

    Added supabase - still need to add the ContentView section for it

--- ACM/Data/Supabase.swift
+++ ACM/Data/Supabase.swift
@@ -5,3 +5,11 @@
 //  Created by Connor Laber on 6/12/25.
 //
 
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://vjttbcvcyinuuphsbabh.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqdHRiY3ZjeWludXVwaHNiYWJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyNDY1NjcsImV4cCI6MjA2NDgyMjU2N30.Nqb9P2a3RwG8f90euQWPmySnvrXzQZXIdTpv3qTkAzE"
+)
+        
---
+++ ACM/Data/Todo.swift
@@ -0,0 +1,14 @@
+//
+//  Todo.swift
+//  ACM
+//
+//  Created by Connor Laber on 6/12/25.
+//
+
+
+import Foundation
+
+struct Todo: Identifiable, Decodable {
+  var id: Int
+  var title: String
+}
