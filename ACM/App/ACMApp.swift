6ce0bbd959260e152300c2574e1ad7f8b7db33ca
Imago-Dei2024 <connorlaber1@gmail.com>
2025-06-15 14:33:13 -0600

    Update ACMApp.swift

--- ACM/App/ACMApp.swift
+++ ACM/App/ACMApp.swift
@@ -8,27 +8,20 @@
 //  which view to show based on the user's authentication state.
 //
 
+//
+//  ACMApp.swift
+//  ACM
+//
+//  Created by Connor Laber on 6/12/25.
+//
+
 import SwiftUI
 
 @main
 struct ACMApp: App {
-    // Create and manage the lifecycle of the AuthManager.
-    @StateObject private var authManager = AuthManager()
-
     var body: some Scene {
         WindowGroup {
-            // Conditionally show ContentView or AuthenticationView.
-            Group {
-                if authManager.session != nil {
-                    // If a session exists, the user is logged in.
-                    ContentView()
-                } else {
-                    // If there's no session, show the login/signup screen.
-                    AuthenticationView()
-                }
-            }
-            // Provide the AuthManager to the entire view hierarchy.
-            .environmentObject(authManager)
+            ContentView()
         }
     }
 }
